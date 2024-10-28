8064ee5a9e4485cb92ee8437ac412c20e00283d0
hardiksuthar74 <hardiksuthar74@gmail.com>
2024-10-29 00:18:56 +0530

    stash

---
+++ leetcode/268.py
@@ -0,0 +1,5 @@
+def missingNumber(nums):
+    n = len(nums)
+    total_sum = n * (n + 1) // 2  
+    actual_sum = sum(nums)
+    return total_sum - actual_sum
--- sortingAlgo/bubble_sort.py
+++ sortingAlgo/bubble_sort.py
@@ -4,13 +4,18 @@ def bubble_sort(array):
     def helper_func(array,itr):
         if itr == len(array):
             return array
-
+        swapped = False
         for i in range(len(array) - itr - 1):
+            print(array,array[i],array[i + 1])
             if array[i] > array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
+                swapped = True
 
+        if not swapped:
+            return array
+        
         return helper_func(array,itr+1)
 
     return helper_func(array, itr)
 
-print(bubble_sort([241,111,223,45,151,1,19,2,3,45,78]))
+print(bubble_sort([8,1,2,3,4,5,6,7]))
